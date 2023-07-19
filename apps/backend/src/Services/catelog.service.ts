041af80f483ee681a40018aa1d85ebccb7107c5d
Davia Deacon <dd611950@digicelgroup.com>
2023-07-19 15:58:13 -0500

    feat: wrote enough code to pass test

--- apps/backend/src/Services/catelog.service.ts
+++ apps/backend/src/Services/catelog.service.ts
@@ -1,10 +1,16 @@
 import { Catelog } from "src/Entities/Catelog";
 import { ICatelogService } from "src/Interfaces/ICatelogService.interface";
 
-export class CatelogService implements ICatelogService{
+export class CatelogService implements ICatelogService {
+
+    private readonly catelogs: Catelog[] = [];
 
     addCatelog(catelog: Catelog): Catelog {
-        throw new Error("Method not implemented.");
+        this.catelogs.push(catelog);
+        return catelog;
     }
 
+    GetCatelogs(): Catelog[] {
+        throw new Error("Method not implemented.");
+    }
 }
