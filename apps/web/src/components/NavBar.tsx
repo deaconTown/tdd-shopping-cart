322b37bf80a031d88242234dc76956be5fe2bed0
Davia Deacon <dd611950@digicelgroup.com>
2023-09-14 17:29:20 -0500

    added a logo - started over the creation of the navbar - removed all global styling

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -2,44 +2,52 @@ import React from 'react';
 import Image from 'next/image';
 import { useState } from 'react';
 import Link from 'next/link';
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/solid'
+import Logo from '../../images/logo_200x200.png'
+import { Bars3Icon, ShoppingCartIcon } from '@heroicons/react/24/solid'
 
 function NavBar() {
     const [navBar, setNavBar] = useState(false);
     return (
-        <div>
-            <nav className='w-full bg-red fixed top-0 left-0 right-0 z-10'>
-                <div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8</main>'>
-                    <div className=''>
-                        <div className='flex items-center justify-between py-3 md:py-5 md:block'>
-                            {/*LOGO */}
-                            <Link href="/">
-                                <h2 className='text-2xl text-cyan-600 font-bold'>LOGO</h2>
-                            </Link>
-                            {/*HAMBURGER BUTTON FOR MOBILE */}
-                            <div className='md:hidden'>
-                                <button 
-                                className='p-2 text-purple-700 rounded-md outline-none focus:border-pink-400'
-                                onClick={() => setNavBar(!navBar)}
-                                >
-                                    {navBar ? (
-                                        <AdjustmentsHorizontalIcon></AdjustmentsHorizontalIcon>
-                                        // <Image src="/close.svg" alt='logo' height={30} width={30}/>
-                                    ) : (
-                                        <Image src='/hamburger-menu.svg'
-                                        width={30}
-                                        height={30}
-                                        alt='logo'
-                                        className='focus:border-none active:border-none' />
-                                    )}
+        <nav id='navBar' className='fixed h-24 w-full shadow-xl bg-white'>
+            <div
+                id='left-side'
+                className='flex justify-between items-center h-full w-full  px-4 2xl:px16'>
+                <Link href='/'>
+                    <Image
+                        src={Logo}
+                        alt='Logo'
+                        width={75}
+                        height={75}
+                        className=' border-orange-300 border border-opacity-50 rounded-3xl cursor-pointer'
+                        priority
+                    />
+                </Link>
+                <div id='right-side'>
+                    <ul id='menu-list' className='hidden sm:flex'>
+                        <Link href="">
+                            <li className='ml-10 uppercase hover:border-b text-xl'>Home</li>
+                        </Link>
 
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                        <Link href="">
+                            <li className='ml-10 uppercase hover:border-b text-xl'>Contact Us</li>
+                        </Link>
+
+                        <Link href="">
+                            <li className='ml-10 uppercase hover:border-b text-xl'>About</li>
+                        </Link>
+
+                        <Link href="">
+                            {/* <li className='ml-10 uppercase hover:border-b text-xl'>Cart</li> */}
+                            <ShoppingCartIcon className='h-6 w-6 ml-10 uppercase hover:border-b text-xl' />
+                        </Link>
+                    </ul>
                 </div>
-            </nav>
-        </div>
+                <div id='hamburger' className='md:hidden cursor-pointer pl-24'>
+                    <Bars3Icon className='h-6 w-6' />
+                </div>
+            </div>
+
+        </nav>
     )
 }
 
