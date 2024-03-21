2c9892743083b5fc62e009dd6333e537b27eec95
Davia Deacon <dd611950@digicelgroup.com>
2024-03-20 22:28:08 -0500

    feat(email-notification-service): imported the new modules into the appModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailModule } from './email/email.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
+  imports: [EmailModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
